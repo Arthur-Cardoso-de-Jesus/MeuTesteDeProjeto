d6cc0d26e0fb8554c86d1c17cace243d72bd5659
Arthur Cardoso de Jesus <arthurcardosoj@gmail.com>
2021-04-22 15:21:16 -0300

    Projeto Criado dia 22/04/2021 - Meu teste de como subir meu projeto pro GitHub - foi criado no main o comando SOUT () - MINHA MENSAGEM

---
+++ src/projetogithub/ProjetoGithub.java
@@ -0,0 +1,17 @@
+
+package projetogithub;
+
+/**
+ *
+ * @author baung
+ */
+public class ProjetoGithub {
+
+    /**
+     * Author:Arthur Cardoso de Jesus
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World! -O meu primeiro projeto do itHub");
+    }
+    
+}
